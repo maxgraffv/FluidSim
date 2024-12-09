5cdd5150f8482d0c3423dc709069ea606fa5a0f2
Max Graff <maksymilian.graff@gmail.com>
2024-12-09 17:38:12 +0100

    specified units for every type in comment

--- Materials/Material.h
+++ Materials/Material.h
@@ -22,18 +22,18 @@ class Material
         float pressure; // Pa
         float temperature; // K
 
-        float velocity;
+        float velocity; // m/s
         float velocity_vec_x;
         float velocity_vec_y;
         float velocity_vec_z;
 
-        float pos_x;
-        float pos_y;
-        float pos_z;
+        float pos_x; //m
+        float pos_y; //m
+        float pos_z; //m
 
-        float center_x;
-        float center_y;
-        float center_z;
+        float center_x; //m
+        float center_y; //m
+        float center_z; //m
 
         Material();
 
