6bdb13481a6d6a6b5f009b0c37397585abfc12a5
Max Graff <maksymilian.graff@gmail.com>
2024-12-09 18:43:39 +0100

    Tensor declared as 3d tensor only, vector, get, set and Constructor declared

---
+++ Tensor/tensor.h
@@ -0,0 +1,44 @@
+#ifndef TENSOR_H
+#define TENSOR_H
+
+#include <vector>
+
+template <typename T>
+class Tensor
+{
+
+    private:
+
+
+
+    public:
+
+        /*
+            !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+            AT THE MOMENT TENSOR IS 3D ONLY
+
+            !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        */
+
+        std::vector<T> data;
+
+        Tensor(int x, int y, int z);
+        T* get(int x, int y, int z);
+        void set(int x, int y, int z, T);
+         
+
+
+
+
+
+
+
+
+};
+
+
+
+
+
+#endif
